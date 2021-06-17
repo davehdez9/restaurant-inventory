fc0aa75c5aaaa3462f4fb161adae94906adf4251
David <david@Davids-MacBook-Pro.local>
2021-06-16 22:19:32 -0400

    Styling Sign up form

--- forms.py
+++ forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, FloatField, SelectField
 from wtforms.validators import InputRequired, Email, Length, ValidationError, DataRequired
+from wtforms.widgets import Input
 
 class SignUpForm(FlaskForm):
     """Form for adding users."""
-    username = StringField("username", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired(), 
+                                                   Length(min=5, max=35, message="Username must be between %(min)d and %(max)dcharacters")], 
+                                                   render_kw={"placeholder": "frodo123"})
+    email = StringField("Email", validators=[DataRequired(), Email()], render_kw={"placeholder": "frodo123@example.com"})
     password = PasswordField("Password", validators=[Length(min=6)])
     
 class LoginForm(FlaskForm):
